7eb84cc9308e59ccc6c1e23412a9adc604795668
Lukasz Kurczab <lukasz.kurczab@gmail.com>
2024-12-02 17:15:19 +0100

    feat(navigationList): Created NavigationList feature

---
+++ src/feature/NavigationList/index.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { FC } from "react";
+import { DndContext, closestCenter, DragEndEvent } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import NavigationItem from "../NavigationItem";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store/store";
+import { REORDER_ITEMS } from "@/store/slices/navigationSlice";
+import { INavigationItem } from "@/types/types";
+import {
+  restrictToParentElement,
+  restrictToVerticalAxis,
+} from "@dnd-kit/modifiers";
+
+interface NavigationListProps {
+  items: INavigationItem[] | undefined;
+  parentId?: string | undefined;
+  children?: React.ReactNode;
+}
+
+const NavigationList: FC<NavigationListProps> = ({
+  items,
+  parentId,
+  children,
+}) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (items && over && active.id !== over.id) {
+      const oldIndex = items.findIndex((item) => item.id === active.id);
+      const newIndex = items.findIndex((item) => item.id === over.id);
+      const reorderedItems = arrayMove(items, oldIndex, newIndex);
+      console.log(parentId);
+      dispatch(
+        REORDER_ITEMS({
+          items: reorderedItems,
+          parentId,
+        })
+      );
+    }
+  };
+
+  return (
+    <>
+      {items && (
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+          modifiers={[restrictToParentElement, restrictToVerticalAxis]}
+        >
+          <SortableContext
+            items={items.map((item) => item.id)}
+            strategy={verticalListSortingStrategy}
+          >
+            <ul className="space-y-2">
+              {items.map((item) => (
+                <NavigationItem key={item.id} item={item} />
+              ))}
+            </ul>
+          </SortableContext>
+        </DndContext>
+      )}
+      {children}
+    </>
+  );
+};
+
+export default NavigationList;
